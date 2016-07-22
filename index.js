0eb14b3072bdbd27054d0ebd8e5071261f7d0b23
Jack Perkins <jackaperkins@gmail.com>
2016-07-21 17:34:00 -0700

    integrate db with main timer loop

--- index.js
+++ index.js
@@ -4,32 +4,40 @@ var tail = new Tail('/var/log/keystroke.log');
 var express = require('express');
 var app = express();
 
+var db = require('./db.js');
+
+
 // --- keylogger portion
 var minutes = [];
 var count = 0;
 
-setInterval(function () {
-  minutes.push(count);
-  count = 0;
-  console.log("");
-  console.log(minutes.length + " minutes recorded so far");
-  console.log("last 30 minutes:");
-  for (var i = minutes.length -1; i > minutes.length - 30 && i >= 0; i--){
-    console.log(minutes[i]);
-  }
-}, 1000 * 60);
+db.init(function (err) {
+  if(err) throw err;
 
-tail.on("line", function(data) {
-  //console.log(data);
-  count++;
+  setInterval(function () {
+    db.store({
+      time: new Date().toISOString(),
+      count: count
+    });
+    count = 0;
+    console.log("minute stored");
+    db.get(function (err, data) {
+      console.log(data);
+    });
+  }, 1000 * 60);
+
+  tail.on("line", function(data) {
+    count++;
+  });
+
+  // ------ server portion
+
+  app.get('/minutes', function (req, res) {
+    res.json(minutes);
+  });
+
+  app.use(express.static('public'));
+
+  app.listen(8080);
+
 });
-
-// ------ server portion
-
-app.get('/minutes', function (req, res) {
-  res.json(minutes);
-});
-
-app.use(express.static('public'));
-
-app.listen(8080);
